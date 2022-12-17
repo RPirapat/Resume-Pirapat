91e73d0a219b50c6e38359c2cd2fd1a324df784f
Pirapat Runepen <118962047+RPirapat@users.noreply.github.com>
2022-12-17 17:19:11 +0700

    Frist Projcet Add Bootstrap and Popper

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // ตั้ง Url เริ่มต้น
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  // ตั้ง Url ในการค้นหา
+  { path: 'navbar', component: NavbarComponent},
+  // ตั้ง Url หากใส่ Url มั่ว
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
